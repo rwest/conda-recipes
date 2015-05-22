01103f345b240092a37b97650e8d80eba086ad31
Richard West <r.west@neu.edu>
2015-05-22 10:34:41 -0400

    Added broken fontconfig recipe (from conda's master conda-reipes)

---
+++ fontconfig/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sed s:'@PREFIX@':"$PREFIX":g -i src/fccfg.c
+
+chmod +x configure
+./configure --prefix $PREFIX --enable-libxml2 --disable-docs
+
+make
+make install
+
+# Remove computed cache with local fonts
+rm -Rf $PREFIX/var/cache/fontconfig
+
+# Leave cache directory, in case it's needed
+mkdir -p $PREFIX/var/cache/fontconfig
+touch $PREFIX/var/cache/fontconfig/.leave
